d9cbc386eab4f2774d5557e84f53d568064d21f4
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2022-12-17 18:40:47 +0530

    Old and New Scorce Code
    Old scorce code moved to a seperate folder and new and update scorece added in seperate folder.

---
+++ Update Scorece Code/section17/chain.java
@@ -0,0 +1,5 @@
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
